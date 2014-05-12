121bf2f116ba57b3891e62536ac1369415f78eb5
David Taylor <dmtaylor2011@gmail.com>
2014-05-12 13:43:03 -0700

    Initial Commit.

---
+++ slug_mem.c
@@ -0,0 +1,7 @@
+/* slug_mem.c : An implementation file for the slug memory package
+ * 
+ * A wrapper for malloc and free which does memory diagnostics
+ *
+ * By: Forrest Kerslager, Nick Noto, David Taylor, Kevin Yeap, Connie
+ *       Yu
+ */
---
+++ slug_mem.h
@@ -0,0 +1,7 @@
+/* slug_mem.h : A Header file for the slug memory package
+ * 
+ * A wrapper for malloc and free which does memory diagnostics
+ *
+ * By: Forrest Kerslager, Nick Noto, David Taylor, Kevin Yeap, Connie
+ *       Yu
+ */
