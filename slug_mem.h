53527ac00fd22ba942fa848866591c0b1e0f2dba
David Taylor <dmtaylor2011@gmail.com>
2014-05-12 14:07:39 -0700

    Added Header file stuff.

--- slug_mem.h
+++ slug_mem.h
@@ -5,3 +5,21 @@
  * By: Forrest Kerslager, Nick Noto, David Taylor, Kevin Yeap, Connie
  *       Yu
  */
+
+#ifndef __SLUG_MEM_H__
+#define __SLUG_MEM_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "hashset.h"
+#include "meminfo.h"
+
+void* slug_malloc(size_t size, char* WHERE);
+
+void slug_free(void* addr, char* WHERE);
+
+void slug_memstats(void);
+
+#endif
+
