5a03b11b77964ee494efe79db078bd39a8fe550a
David Taylor <dmtaylor2011@gmail.com>
2014-05-12 15:04:37 -0700

    Initial commit for hashing function for addresses

---
+++ meminfo_hash.c
@@ -0,0 +1,6 @@
+/* meminfo_hash.c : Implementation file for int hashing
+ * By: David Taylor, et. all
+ */
+
+
+/* TODO hash function here */ 
---
+++ meminfo_hash.h
@@ -0,0 +1,6 @@
+/* hash.h : Header file for int hashing
+ * By: David Taylor, et. all
+ */
+
+
+/* TODO Header stuff here */
