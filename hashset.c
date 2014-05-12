8f182d210cae388720c3e026946765e51ca0cda0
David Taylor <dmtaylor2011@gmail.com>
2014-05-12 13:24:26 -0700

    Initial commit
    Forrest's hashset implementation from Course CMPS012B

---
+++ hashset.c
@@ -0,0 +1,130 @@
+// Author: Forrest Kerslager, fkerslag@ucsc.edu
+// $Id: hashset.c,v 1.49 2012-12-07 19:34:20-08 - - $
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "debugf.h"
+#include "hashset.h"
+#include "strhash.h"
+
+#define HASH_NEW_SIZE 15
+
+struct hashset {
+   size_t length;
+   int load;
+   char **array;
+};
+
+hashset_ref new_hashset (void) {
+   hashset_ref new = malloc (sizeof (struct hashset));
+   assert (new != NULL);
+   new->length = HASH_NEW_SIZE;
+   new->load = 0;
+   new->array = malloc (new->length * sizeof (char*));
+   assert (new->array != NULL);
+   for (size_t index = 0; index < new->length; ++index) {
+      new->array[index] = NULL;
+   }
+   DEBUGF ('h', "%p -> struct hashset {length = %d, array=%p}\n",
+                new, new->length, new->array);
+   return new;
+}
+
+void free_hashset (hashset_ref hashset) {
+   DEBUGF ('h', "free (%p), free (%p)\n", hashset->array, hashset);
+   for (size_t i = 0; i < hashset->length; i++){
+      free(hashset->array[i]);
+      hashset->array[i] = NULL;
+   }
+   memset (hashset->array, 0, hashset->length * sizeof (char*));
+   free (hashset->array);
+   memset (hashset, 0, sizeof (struct hashset));
+   free (hashset);
+}
+
+bool too_full_hash(hashset_ref hashset){
+   return hashset->load * 4 > (int)hashset->length;
+}
+
+void double_array_hash(hashset_ref hashset){
+   size_t new_length = 2 * hashset->length + 1;
+   char** new_array = malloc(new_length * sizeof (char*));
+   for (size_t index = 0; index < new_length; ++index) {
+      new_array[index] = NULL;
+   }   
+   
+   for (size_t i = 0; i<hashset->length; i++){
+      bool exists = false;
+      if (hashset->array[i] != NULL){
+         uint32_t index = strhash(hashset->array[i]) % new_length;
+         while (new_array[index] != NULL) {
+            if (strcmp (new_array[index],hashset->array[i]) == 0){
+               exists = true;
+               break;
+            }
+            index = (index + 1) % new_length;
+         }
+         if(!exists) new_array[index] = hashset->array[i];
+      }
+   }
+   
+   free(hashset->array);
+   hashset->length = new_length;
+   hashset->array = new_array;
+}
+
+void put_hashset (hashset_ref hashset, char *item) {
+   if (too_full_hash (hashset)) double_array_hash (hashset);
+   uint32_t index = strhash (item) % hashset->length;
+   while (hashset->array[index] != NULL) {
+      if (strcmp (hashset->array[index], item) == 0) return;
+      index = (index + 1) % hashset->length;
+   }
+   hashset->array[index] = strdup (item);
+   hashset->load++;
+}
+
+bool has_hashset (hashset_ref hashset, char *item) {
+   uint32_t code = strhash (item) % hashset->length;
+   while (hashset->array[code] != NULL) {
+      if (strcmp (hashset->array[code], item) == 0) return true;
+      code = (code + 1) % hashset->length;
+   }
+   return false;
+}
+
+void print_debug(hashset_ref hashset){
+   int size = 0;
+   int count_array[hashset->load];
+   for (int index = 0; index<hashset->load; index++){
+      count_array[index] = 0;
+   }
+   for (size_t index = 0; index < hashset->length; index++){
+      if(hashset->array[index] != NULL) size++;
+      else{
+         count_array[size]++;
+         size = 0;
+      }
+   }
+
+   printf("%15d words in hash set\n", hashset->load);
+   printf("%15d length of the hash array\n", (int)hashset->length);
+   for (int index = 1; index<hashset->load; index++){
+      if (count_array[index] != 0)
+          printf("%15d clusters of size %d\n",count_array[index],index);
+   }
+}
+
+void print_hash(hashset_ref hashset){
+   for (size_t index = 0; index < hashset->length; index++){
+      if(hashset->array[index] != NULL){
+         uint32_t code = strhash (hashset->array[index]);
+         printf("array[%10d] = %12u \"%s\"\n",
+                (int)index, code, hashset->array[index]);
+      }
+   }
+}
+
