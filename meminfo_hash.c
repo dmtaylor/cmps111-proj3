80b512d34ff2292f7a5fc3437f020f31a3547833
David Taylor <dmtaylor2011@gmail.com>
2014-05-12 15:24:17 -0700

    Added a rudimentary hashing function. CHECK THIS!!

--- meminfo_hash.c
+++ meminfo_hash.c
@@ -1,6 +1,11 @@
 /* meminfo_hash.c : Implementation file for int hashing
  * By: David Taylor, et. all
  */
+/* Check this hash function */
 
+uint32_t meminfo_hash(void* item){
+    uint32_t itemval = (uint32_t) item;
+    uint32_t returnval = item * (item +1) % 81181;
+    return returnval;
+}
 
-/* TODO hash function here */ 
--- meminfo_hash.h
+++ meminfo_hash.h
@@ -1,6 +1,14 @@
-/* hash.h : Header file for int hashing
+/* meminfo_hash.h : Header file for int hashing
  * By: David Taylor, et. all
  */
 
 
 /* TODO Header stuff here */
+
+#ifndef __MEMINFO_HASH_H__
+#define __MEMINFO_HASH_H__
+
+uint32_t meminfo_hash(void* item);
+
+#endif
+
