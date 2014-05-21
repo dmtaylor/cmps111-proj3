42fb5b53ffafa56e6eb41074f15661e2e8f6cfed
fkerslag <fkerslag@ucsc.edu>
2014-05-21 12:21:06 -0700

    Test5 finished

--- tests/test5.c
+++ tests/test5.c
@@ -2,7 +2,7 @@
 	Show that after allocating and deallocating memory, trying to deallocate a valid region 
 	by passing in a pointer inside the region is immediately detected.
 
-	THIS TEST IS NOT COMPLETE
+	This test will print out each pointer, exit on an error and then print a table and statistics
 */
 
 #include <stdio.h>
@@ -12,6 +12,17 @@
 #include "slug_mem.h"
 
 int main(int argc, char *argv[]) {
+	double *p, *p2;
+
+	p = malloc(sizeof(double)); 
+
+	p2 = p + 1;
+
+	printf("p = %p	p2 = %p\n",p,p2);
+
+	free(p2);
+
+	printf("Done\n");
 
 	return 0;
 }
