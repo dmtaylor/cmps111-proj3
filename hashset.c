97b8f6de20d6a6332a423aefd67756c125391575
David Taylor <dmtaylor2011@gmail.com>
2014-05-21 11:56:46 -0700

    Added documentation and eliminated an extra declaration

--- hashset.c
+++ hashset.c
@@ -3,6 +3,13 @@
  * 
  * By: Forrest Kerslager, Nick Noto, David Taylor, Kevin Yeap, 
  *     Connie Yu.
+ * 
+ * NOTE: This hash table implementation was originally designed for
+ * course CMPS 012B, as a hash table for strings. It has since been
+ * modified for use on our custom memory structs. Most of it has been
+ * re-written, but the structure of the module remains more or less the
+ * same, as well as the fundamental algorithm.
+ * 
  */
 
 #include <assert.h>
@@ -17,6 +24,10 @@
 #define HASH_NEW_SIZE 15
 
 
+/* hashset_ref new_hashset (void)
+ * Creates a new empty hash table
+ * This is pretty self-explainatory 
+ * */
 hashset_ref new_hashset (void) 
 {
 	size_t index;
@@ -36,10 +47,14 @@ hashset_ref new_hashset (void)
 	return new;
 }
 
+/* void free_hashset (hashset_ref hashset)
+ * This frees the given hash table, also self explainatory
+ * 
+ * */
 void free_hashset (hashset_ref hashset) 
 {
 	size_t i;
-
+	/* Frees all entries in the table */
 	for (i = 0; i < hashset->length; i++){
 		free(hashset->array[i]);
 		hashset->array[i] = NULL;
@@ -51,12 +66,23 @@ void free_hashset (hashset_ref hashset)
 	free (hashset);
 }
 
+/* bool too_full_hash(hashset_ref hashset)
+ * Determines if the size of the table is too small for the number of
+ * entries.
+ * */
 bool too_full_hash(hashset_ref hashset)
 {
    return hashset->load * 4 > (int)hashset->length;
 }
 
 
+/* void double_array_hash(hashset_ref hashset)
+ * Doubles the size of the hashtable. It creates a new hashtable, and
+ * copies over the entries in the old table, then frees the old table.
+ * This function gets periodically called when the number of entries in
+ * the hash table gets large relative to the table.
+ * 
+ * */
 void double_array_hash(hashset_ref hashset)
 {
 	size_t i;
@@ -64,13 +90,14 @@ void double_array_hash(hashset_ref hashset)
 	uint32_t index;
 	size_t new_length = 2 * hashset->length + 1;
 
+	/* Allocates new table */
 	meminfo_ref* new_array = malloc(new_length * sizeof (meminfo_ref));
 	for (index = 0; index < new_length; ++index) {
 		new_array[index] = NULL;
 	}   
-
+	/* Copies each entry in the old hash table into the new one */
 	for (i = 0; i<hashset->length; i++) {
-		bool exists = false;
+		exists = false;
 		if (hashset->array[i] != NULL) {
 			/* Deletes unused meminfos when resizing array */
 			if(hashset->array[i]->tombstone) {
@@ -79,7 +106,7 @@ void double_array_hash(hashset_ref hashset)
 			}
 
 			index = meminfo_hash(hashset->array[i]->address) % new_length;
-
+			/* Checks for any duplicate copies in the new table */
 			while (new_array[index] != NULL) {
 				if (new_array[index]->address == hashset->array[i]->address  && !hashset->array[index]->tombstone) {
 					exists = true;
@@ -93,12 +120,16 @@ void double_array_hash(hashset_ref hashset)
 		}
 	}
 
+	/* Frees old array and sets up new one */
 	free(hashset->array);
 	hashset->length = new_length;
 	hashset->array = new_array;
 }
 
-
+/* meminfo_ref has_hashset (hashset_ref hashset, void* address)
+ * Looks up a given memory address in the hash table, returns the
+ * address of the meminfo struct if found, NULL otherwise. 
+ * */
 meminfo_ref has_hashset (hashset_ref hashset, void* address) 
 {
 	uint32_t code = meminfo_hash (address) % hashset->length;
@@ -112,20 +143,26 @@ meminfo_ref has_hashset (hashset_ref hashset, void* address)
 	return NULL;
 }
 
-
+/* void put_hashset (hashset_ref hashset, meminfo_ref item)
+ * Inserts a given meminfo struct into the hash table
+ */
 void put_hashset (hashset_ref hashset, meminfo_ref item) 
 {
 	uint32_t index;
 
+	/* Doubles the size of the table if there are too many entries */
 	if (too_full_hash (hashset)) {
 		double_array_hash (hashset);
 	}
 
+	/* If the address is already allocated, then it is already in the
+	 * table. We need to free the duplicate copy of the info */
 	if(has_hashset(hashset, item->address) != NULL) {
 		free(item);
 		return;
 	}
 
+	/* Inserts it into the next available appropriate spot */
 	index = meminfo_hash (item->address) % hashset->length;
 	while (hashset->array[index] != NULL) {
 		if(hashset->array[index]->tombstone) {
@@ -139,7 +176,12 @@ void put_hashset (hashset_ref hashset, meminfo_ref item)
 	hashset->load++;
 }
 
-
+/* void remove_hashset (hashset_ref hashset, void* address)
+ * Removes an entry from the hash table
+ * As the table resolves collisions by linear probing, it does no
+ * freeing of structs here. Instead, it sets a tombstone flag which
+ * marks the struct for deletion at an appropriate time. 
+ * */
 void remove_hashset (hashset_ref hashset, void* address)
 {
 	uint32_t index;
@@ -161,7 +203,9 @@ void remove_hashset (hashset_ref hashset, void* address)
 	}
 }
 
-
+/* void print_hash(hashset_ref hashset)
+ * Prints the contents of the hash table
+ * */
 void print_hash(hashset_ref hashset)
 {
 	size_t index;
